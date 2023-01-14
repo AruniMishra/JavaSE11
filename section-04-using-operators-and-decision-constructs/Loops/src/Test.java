a2b9955469d3bd854df04a2889e0e94934cd0af6
Aruni Mishra <arunimishramsit@gmail.com>
2023-01-14 22:41:58 +0530

    java

--- section-04-using-operators-and-decision-constructs/Loops/src/Test.java
+++ section-04-using-operators-and-decision-constructs/Loops/src/Test.java
@@ -1,7 +1,12 @@
 public class Test {
+    static int testVariable;
 
     public static void main(String[] args) {
-
-        //for(;;); // infinite loop
+        int i, j;
+        for (i = 100, j = 0; i <= 300; i += 90, j++) { // Line 1
+            testVariable = i % 50;   //  Line 2
+            System.out.print(testVariable + " "); // Line 3
+        }
+        System.out.println(j);
     }
 }
