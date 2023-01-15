bd43e835624ea34e5f5c261a2a34d2415e947315
Aruni Mishra <arunimishramsit@gmail.com>
2023-01-15 20:49:36 +0530

    java

--- section-05-working-with-java-arrays/Arrays/src/ArraySearch.java
+++ section-05-working-with-java-arrays/Arrays/src/ArraySearch.java
@@ -1,5 +1,5 @@
-
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class ArraySearch {
@@ -10,19 +10,18 @@ public class ArraySearch {
                 "pqr", "stu", "vwx", "yz"
         };
         String[] firstStringUnsortedDuplicates = {
-                "mno", "jkl", "pqr", "stu", "vwx",
+                "jkl", "mno", "pqr", "stu", "vwx",
                 "yz", "jkl", "abc", "def", "ghi"
         };
 
         String[] partialFirstString = {"abc", "def", "ghi", "jkl", "mno"};
 
         // Create Lists from the arrays to test List search methods
-        List firstList = List.of(firstString);
-        List secondList = Arrays.asList(firstStringUnsortedDuplicates);
+        List<String> firstList = List.of(firstString);
+        List<String> secondList = Arrays.asList(firstStringUnsortedDuplicates);
 
         System.out.println("---------- Arrays binarySearch  ----------");
         // binary search on array, look for "jkl" which is in array
-        String searchString = "jkl";
         System.out.println("Arrays.binarySearch(firstString,\"jkl\") = "
                 + Arrays.binarySearch(firstString, "jkl"));
 
@@ -47,7 +46,7 @@ public class ArraySearch {
         // Try another example...
         System.out.println("Arrays.mismatch(firstStringUnsortedDuplicates," +
                 " new String[]{\"jkl\",\"mno\"}) = "
-                + Arrays.mismatch(firstStringUnsortedDuplicates, new String[]{"jkl", "mno"}));
+                + Arrays.mismatch(firstStringUnsortedDuplicates, new String[]{"mno", "jkl"}));
 
         System.out.println("\n-------------- List methods  -------------------");
         // Does array contain "def"?
@@ -61,13 +60,13 @@ public class ArraySearch {
         // create a second list not in same order
         String[] anotherUnsortedSet = {"jkl", "def", "abc", "ghi", "mno"};
         System.out.println("firstList.containsAll(Arrays.asList(anotherUnsortedSet)) =  "
-                + firstList.containsAll(Arrays.asList(anotherUnsortedSet)));
+                + new HashSet<>(firstList).containsAll(Arrays.asList(anotherUnsortedSet)));
 
         // Use indexOf to get first matching element
         System.out.println("secondList.indexOf(\"jkl\") =  "
                 + secondList.indexOf("jkl"));
 
-        // Use lastIndexOf to to get last matching element
+        // Use lastIndexOf to get last matching element
         System.out.println("secondList.lastIndexOf(\"jkl\") =  "
                 + secondList.lastIndexOf("jkl"));
 
