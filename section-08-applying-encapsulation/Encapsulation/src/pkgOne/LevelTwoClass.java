80a38ccd9a32f7aa98d321df4c1d904e2ddca38a
Aruni Mishra <arunimishramsit@gmail.com>
2023-01-23 16:23:14 +0530

    Java

---
+++ section-08-applying-encapsulation/Encapsulation/src/pkgOne/LevelTwoClass.java
@@ -0,0 +1,30 @@
+package pkgOne;
+
+class LevelOneClass {
+    // Protected access constructor
+    protected LevelOneClass() {
+        System.out.println("protected LevelOneClass " +
+                "no args constructor");
+    }
+
+    // Package-private access constructor
+    LevelOneClass(String text) {
+        System.out.println("package level LevelOneClass " +
+                "single params constructor");
+    }
+}
+
+public class LevelTwoClass extends LevelOneClass {
+
+    // Protected access constructor
+    protected LevelTwoClass() {
+        System.out.println("protected LevelTwoClass " +
+                "no args constructor");
+    }
+
+    // Package-private access constructor
+    LevelTwoClass(String text) {
+        System.out.println("package level LevelTwoClass " +
+                "single params constructor");
+    }
+}
