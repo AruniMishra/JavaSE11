282a7c03a85948bfad676b67e4ec541b8e9c6169
Aruni Mishra <arunimishramsit@gmail.com>
2023-02-03 00:34:42 +0530

    Java

---
+++ section-09-resusing-implementations-through-inheritance/Polymorphism/src/OverrideExample.java
@@ -0,0 +1,39 @@
+class BaseClass {
+    public void goodMethod() {
+        System.out.println("BaseClass executing good method");
+    }
+
+    public void printInformation(CharSequence s) {
+        System.out.println("BaseClass prints " + s);
+    }
+
+    public CharSequence getInformation() {
+        return getClass().getName();
+    }
+}
+
+class ExtendedClass extends BaseClass {
+
+    public void goodMethod() {
+        super.goodMethod();
+        System.out.println("AND ExtendedClass executing a better method");
+    }
+
+    //not an override method
+    public void printInformation(String string) {
+        System.out.println("ExtendedClass prints " + string);
+    }
+
+    public String getInformation() {
+        return getClass().getName();
+    }
+}
+
+public class OverrideExample {
+    public static void main(String[] args) {
+        ExtendedClass e = new ExtendedClass();
+        e.goodMethod();
+        e.printInformation(e.getInformation());
+        e.printInformation((CharSequence) e.getInformation());
+    }
+}
