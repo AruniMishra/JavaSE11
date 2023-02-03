1db63de1b54091ce30a93edcfded7518fb39b540
Aruni Mishra <arunimishramsit@gmail.com>
2023-02-03 17:43:55 +0530

    Java

--- section-09-resusing-implementations-through-inheritance/Polymorphism/src/OverrideExample.java
+++ section-09-resusing-implementations-through-inheritance/Polymorphism/src/OverrideExample.java
@@ -14,13 +14,19 @@ class BaseClass {
 
 class ExtendedClass extends BaseClass {
 
+    // note: an override can still "throws" different type of Exception
     public void goodMethod() {
         super.goodMethod();
         System.out.println("AND ExtendedClass executing a better method");
     }
 
+    public void extendedmethod() {
+        System.out.println("extended-method invoked");
+    }
+
     //not an override method
-    public void printInformation(String string) {
+    //hence can also be declared as private (weaker privilege)
+    private void printInformation(String string) {
         System.out.println("ExtendedClass prints " + string);
     }
 
@@ -35,5 +41,15 @@ public class OverrideExample {
         e.goodMethod();
         e.printInformation(e.getInformation());
         e.printInformation((CharSequence) e.getInformation());
+
+
+        // note: below is valid
+        BaseClass baseClass = new ExtendedClass();
+        ((ExtendedClass) baseClass).extendedmethod();
+
+
+        // Run time error
+        BaseClass baseClass2 = new BaseClass();
+        ((ExtendedClass) baseClass2).extendedmethod();
     }
 }
