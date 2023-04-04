4a79a59c9e5bd1ef37263b1b20ea23564cfa2fe2
Aruni Mishra <arunimishramsit@gmail.com>
2023-04-04 15:32:11 +0530

    Java

--- part2-section-02-java-fundamentals/Fundamentals/src/demo/TestClass.java
+++ part2-section-02-java-fundamentals/Fundamentals/src/demo/TestClass.java
@@ -1,11 +1,20 @@
 package demo;
 
- class  TestClass {
+class TestClass {
+    static int data = 30;
+    static Inner obj2 = new Inner();
 
-    private static class InnerClass{
-
+    public static void main(String args[]) {
+        TestClass.Inner obj = new TestClass.Inner();
+        obj.msg();
+        Inner obj1 = new Inner();
+        obj.msg();
+        obj2.msg();
     }
 
-
-
+    static class Inner {
+        void msg() {
+            System.out.println("data is " + data);
+        }
+    }
 }
