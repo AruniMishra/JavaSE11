8f1bedfec2e0fbb4075e822e1ba30207399df134
Aruni Mishra <arunimishramsit@gmail.com>
2023-06-19 16:00:49 +0530

    Java

---
+++ part3-examtopics/Examtopics/src/PersonTest.java
@@ -0,0 +1,16 @@
+public class PersonTest {
+
+    private String name;
+
+    public PersonTest(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonTest{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
--- part3-examtopics/Examtopics/src/Test04.java
+++ part3-examtopics/Examtopics/src/Test04.java
@@ -20,5 +20,39 @@ public class Test04 {
         System.out.println(sb + " :: sb.length():" + sb.length() + ", sb.capacity(): " + sb.capacity());
 
 
+        int[][][] e = {{new int[]{1}, new int[]{1}, new int[]{1}}, {new int[]{2}, new int[]{2}, new int[]{2}}};
+
+        int[][] e1 = {{1, 1, 1}, {2, 2, 2}};
+
+        short sh = (short) 'A';
+
+        float x = 1f;
+
+        byte b = 105;
+        char c = (char) b;
+
+        String contact = "(+2) (999) (232)";
+
+        int x1 = 12_34;
+
+
+
+
+        PersonTest personTest = new PersonTest("Joe");
+        System.out.println(checkPerson(personTest));
+        personTest = null;
+        System.out.println(checkPerson(personTest));
+
+    }
+
+    public static PersonTest checkPerson(PersonTest s) {
+        if (s == null) {
+            s = new PersonTest("Mary");
+        } else {
+            s = null;
+        }
+
+        return s;
+
     }
 }
