07447c3299d81b3cfa5251018e1e0a687ca8161e
Aruni Mishra <arunimishramsit@gmail.com>
2023-08-16 22:47:52 +0530

    Java

--- part2-section-12-concurrency/Concurrency/src/SynchronizedMethod.java
+++ part2-section-12-concurrency/Concurrency/src/SynchronizedMethod.java
@@ -27,60 +27,96 @@ class TheBallInTheAir {
     // Making it volatile alone doesn't solve the problem.
     // private volatile int hit;
 
+
+    // option2
     private AtomicInteger hit = new AtomicInteger(0);
 
 
-    // Map of players and their hits..
+    // Map of players and their hits.. (option1 & option2)
     // private Map<String, Integer> players = new TreeMap<>();
 
+
+    /// option3
     private Map<String, Integer> players = Collections.synchronizedMap(new TreeMap<>());
 
 
-
     // Constructor
-    // prevents race condition
-    TheBallInTheAir() {
-        players.put("Jane", 0);
-        players.put("Mary", 0);
-        players.put("Ralph", 0);
-        players.put("Joe", 0);
-    }
+    // prevents race condition-better code for option3
+    // TheBallInTheAir() {
+    //     players.put("Jane", 0);
+    //     players.put("Mary", 0);
+    //     players.put("Ralph", 0);
+    //     players.put("Joe", 0);
+    // }
 
     // Method called by threads, adds player to map if not found
     // or increments value in map
     public int addHit(String player) {
-        // public synchronized int addHit(String player) { // valid, but not performance efficient
 
+    /*
+    making it synchronized (without volatile int hit)
+    but there is overhead when using synchronized methods
+    and all your other threads are blocked while the synchronized method executes.
+    */
+    // public synchronized int addHit(String player) { // valid- - option1, but not performance efficient
+
+        //------
+        /*
         // this.hit++;
 
-        // synchronized (this) {
-        //     this.hit++;  // better than making entire block as synchronized, but only fixed Total Volleyball hits
-        // }
+        // better than making entire block as synchronized,
+        // but only fixed Total Volleyball hits
+        synchronized (this) {
+            this.hit++;
+        }
 
-        synchronized (players) { // valid and concrete
+        if (players.containsKey(player)) {
+            players.computeIfPresent(player, (key, val) -> ++val);
+        } else {
+            players.put(player, 1);
+        }
+
+
+        return this.hit;
+        */
+
+        //-------
+
+
+        //-- with Atomic option2
+        /*
+        synchronized (players) { // valid and concrete- option2
             if (players.containsKey(player)) {
                 players.computeIfPresent(player, (key, val) -> ++val);
             } else {
-                // players.put(player, 1);
-                // A null is returned if no mapping was found
-                if (players.putIfAbsent(player, 1) != null) { // another thread might have added the entry.
-                    System.out.println("Encountered race condition");
-                    players.computeIfPresent(player, (key, val) -> ++val);
-                }
+                players.put(player, 1);
             }
-            // return this.hit;
         }
         return hit.incrementAndGet();
 
+         */
+        //-- with Atomic
+
+
+        if (players.containsKey(player)) {
+            players.computeIfPresent(player, (key, val) -> ++val);
+        } else {
+            players.put(player, 1);
+            // A null is returned if no mapping was found
+            if (players.putIfAbsent(player, 1) != null) { // another thread might have added the entry.
+                System.out.println("Encountered race condition");
+                players.computeIfPresent(player, (key, val) -> ++val);
+            }
+        }
+        return hit.incrementAndGet();
     }
 
     // Present writeable output
     public String toString() {
-        return "Total " + ballType + " hits: " +
-                // this.hit
-                this.hit.get() // with Atomic
-                + ", Player hits: " + players.values().stream().mapToInt(s -> s).sum()
-                + "\nPlayers List: " + players;
+        return "Total " + ballType + " hits: "
+                // + this.hit
+                + this.hit.get() // with Atomic
+                + ", Player hits: " + players.values().stream().mapToInt(s -> s).sum() + "\nPlayers List: " + players;
     }
 }
 
@@ -94,10 +130,7 @@ public class SynchronizedMethod {
         String[] players = {"Jane", "Mary", "Ralph", "Joe"};
         Random r = new Random();
         // random list of player names, representing their turn to hit ball
-        List<String> playerTurns =
-                Stream.generate(() -> players[r.nextInt(4)])
-                        .limit(100)
-                        .collect(Collectors.toList());
+        List<String> playerTurns = Stream.generate(() -> players[r.nextInt(4)]).limit(100).collect(Collectors.toList());
         // playerTurns.forEach(System.out::println);
 
         ExecutorService executorService = null;
