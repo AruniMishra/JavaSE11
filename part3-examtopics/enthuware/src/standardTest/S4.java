401a04206ea4a8ebf461156297d153ff5068ee1d
Aruni Mishra <arunimishramsit@gmail.com>
2023-09-13 18:12:27 +0530

    Java

--- part3-examtopics/enthuware/src/standardTest/S4.java
+++ part3-examtopics/enthuware/src/standardTest/S4.java
@@ -134,3 +134,19 @@ class Book {
     }
 }
 
+
+class s4TestClass18{
+    public void method(Object o){
+        System.out.println("Object Version");
+    }
+    // public void method(java.io.FileNotFoundException s){
+    //     System.out.println("java.io.FileNotFoundException Version");
+    // }
+    // public void method(java.io.IOException s){
+    //     System.out.println("IOException Version");
+    // }
+    public static void main(String args[]){
+        s4TestClass18 tc = new s4TestClass18();
+        tc.method(null);
+    }
+}
