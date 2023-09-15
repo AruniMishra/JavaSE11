858707131acda61222a34b8a43dc4409bdab0274
Aruni Mishra <arunimishramsit@gmail.com>
2023-09-15 19:06:42 +0530

    Java

--- part3-examtopics/enthuware/src/standardTest/S4.java
+++ part3-examtopics/enthuware/src/standardTest/S4.java
@@ -187,3 +187,16 @@ class Calculator {
         return func.apply((int) base);
     }
 }
+
+
+class Test {
+
+    public static void main(String[] args) {
+        var arr = new Boolean[1];
+        if(arr[0]) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
