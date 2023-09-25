0a0985097cb1392995db743e9e42a2e9e109c403
Aruni Mishra <arunimishramsit@gmail.com>
2023-09-25 18:18:39 +0530

    Java

--- part3-examtopics/enthuware/src/standardTest/S6.java
+++ part3-examtopics/enthuware/src/standardTest/S6.java
@@ -3,6 +3,7 @@ package standardTest;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 public class S6 {
 }
@@ -16,5 +17,28 @@ class s6T22 {
         col.add(2); // 2
         var list2 = List.copyOf(col); // 3
         System.out.println(list1 + ", " + list2);
+    }
+}
+
+class s6T48FunWithOptional {
+    public static String getValue() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        // Optional.of method throws NullPointerException if you try to create an Optional with a null value.
+        // If you expect the argument to be null, you should use Optional.ofNullable method,
+        // which returns an empty Optional if the argument is null.
+        Optional<String> stro = Optional.of(getValue());// 1
+
+        System.out.println(stro.isPresent());// 2
+
+        // Calling get() on an empty Optional causes a java.util.NoSuchElementException to be thrown.
+        // If you expect an Optional to be empty, you should use orElse(T ) method.
+        System.out.println(stro.get());// 3
+
+        // This method never throws any exception, not even if the argument is null.
+        System.out.println(stro.orElse(null));// 4
     }
 }
