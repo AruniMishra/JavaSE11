f5ee5dce0ad6ddf825ab62b8b320c4e50b3be030
aruni mishra <arunimishramsit@gmail.com>
2023-10-07 19:31:12 +0530

    Java.

--- part3-examtopics/enthuware/src/standardTest/S9.java
+++ part3-examtopics/enthuware/src/standardTest/S9.java
@@ -1,6 +1,13 @@
 package standardTest;
 
 public class S9 {
+    public static void main(String[] args) {
+        double daaa[][][] = new double[3][][];
+        var d = 100.0;
+        double[][] daa = new double[1][1];
+
+        // daa[1][1] = d;
+    }
 }
 
 class NewExceptionT43 extends Exception {
@@ -27,4 +34,25 @@ class ExceptionTestT43 {
         throw new AnotherExceptionT43();
     }
 
+}
+
+
+class s10TableTest {
+    static String[][] table;
+    public static void main(String[] args) {
+        String[] x = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+        String[] y1 = { "1", "2", "3", "4", "5" };
+        String[] y2 = { "a", "b", "c" };
+
+        table = new String[3][];
+        table[0] = x;
+        table[1] = y1;
+        table[2] = y2;
+
+        //INSERT CODE HERE
+        for(var row : table){
+            System.out.print(row[row.length-1]);
+        }
+
+    }
 }
