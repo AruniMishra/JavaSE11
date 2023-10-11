5f075b274693573fa6a9880262a6c6244966eb47
Aruni Mishra <arunimishramsit@gmail.com>
2023-10-11 16:06:04 +0530

    Java

--- part3-examtopics/udemy/src/PT6_arrays_collection_2/Test.java
+++ part3-examtopics/udemy/src/PT6_arrays_collection_2/Test.java
@@ -16,3 +16,29 @@ class Test3 {
         List<String> list5 = new ArrayList<>(); // Line n9
     }
 }
+
+class Test14 {
+    public static void main(String[] args) {
+        /*
+        In this case `animals.remove(new StringBuilder("Alligator"));` will never remove any items from the list as
+         StringBuilder class doesn't override the equals(Object) method of Object class.
+
+        StringBuilder instances created at "animals.add(new StringBuilder("Alligator"));"
+        and "animals.remove(new StringBuilder("Alligator"));"
+        are at different memory locations and equals(Object) method returns false for these instances.
+         */
+        List<StringBuilder> animals = new ArrayList<>();
+        animals.add(new StringBuilder("Walrus"));
+        animals.add(new StringBuilder("Anaconda"));
+        animals.add(new StringBuilder("Alligator"));
+        animals.add(new StringBuilder("Dog"));
+
+        for(int i = 0; i < animals.size(); i++) {
+            if(i == 0) {
+                animals.remove(new StringBuilder("Alligator"));
+            }
+        }
+
+        System.out.println(animals);
+    }
+}
